8d4313a86adc3fe463468c0213f883e86f30ef89
ShirleyFennette <152020270+ShirleyFennette@users.noreply.github.com>
2025-07-02 16:06:42 +0300

    Create exec-tests.sh

---
+++ exec-tests.sh
@@ -0,0 +1,9 @@
+export ALLURE_TOKEN=67aad6fa-966a-49ef-a598-e572b3e030f9
+export ALLURE_ENDPOINT=http://testing.qatools.cloud
+export ALLURE_PROJECT_ID=1605
+export ALLURE_LAUNCH_NAME="checking various attachments"
+export ALLURE_RESULTS=allure-results
+
+./allurectl watch -- pytest ./tests --alluredir=${ALLURE_RESULTS}
+--endpoint http://testing.qatools.cloud
+--token 67aad6fa-966a-49ef-a598-e572b3e030f9 
